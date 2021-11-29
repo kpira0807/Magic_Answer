c10f6d16217f40ec8cabd450024f9ad63cc60326
Iryna <iryna.kopchynska@gmail.com>
2021-11-29 16:06:17 +0200

    Refactoring Realm to background

--- MagicAnswer/MagicAnswer/Services/HistoryStorage.swift
+++ MagicAnswer/MagicAnswer/Services/HistoryStorage.swift
@@ -6,28 +6,36 @@ final class HistoryStorage {
         let object = PresentedAnswer()
         object.date = Date()
         object.message = answer
-        do {
-            let realm = try Realm()
-            try realm.write {
-                realm.add(object)
+        DispatchQueue(label: "background").async {
+            autoreleasepool {
+                do {
+                    let realm = try Realm()
+                    try realm.write {
+                        realm.add(object)
+                        realm.refresh()
+                    }
+                } catch {
+                    print("Error")
+                }
             }
-        } catch {
-            print("Error")
         }
     }
     
     var results: Results<PresentedAnswer>!
     
     func getGivenAnswer() -> [PresentedAnswer] {
-        do {
-            let realm = try Realm()
-            let answers = realm.objects(PresentedAnswer.self).sorted(byKeyPath: PresentedAnswer.Property.date.rawValue, ascending: false)
-            let arrayAnswers = Array(answers)
+        DispatchQueue(label: "background").sync {
+            do {
+                let realm = try Realm()
+                let answers = realm.objects(PresentedAnswer.self).sorted(byKeyPath: PresentedAnswer.Property.date.rawValue, ascending: false)
+                realm.refresh()
+                let arrayAnswers = Array(answers)
+                return arrayAnswers
+            } catch {
+                print("Error")
+            }
+            let arrayAnswers = Array(results)
             return arrayAnswers
-        } catch {
-            print("Error")
         }
-        let arrayAnswers = Array(results)
-        return arrayAnswers
     }
 }
